c78e45693ccb139887d02f1c718d44d4f07f80c6
Samerhajj <samerhaj44@gmail.com>
2023-01-04 06:44:41 +0000

    fixed some packages?

---
+++ FrontEnd/termi-project/src/games/GameDC/TrueFalseGame.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TrueFalseGame = () =>{
+    
+    // https://he.y8.com/games/true_false_quiz
+    
+    return
+    (<>
+        <div>
+            
+        </div>
+    </>
+    )
+    
+}
+export default TrueFalseGame;
