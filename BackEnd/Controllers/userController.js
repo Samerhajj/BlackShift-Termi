7c33c3666517091822204f36768d2e2fcb609e20
Samerhajj <samerhaj44@gmail.com>
2023-01-04 05:52:56 +0000

    Removed useless comment

--- BackEnd/Controllers/userController.js
+++ BackEnd/Controllers/userController.js
@@ -61,73 +61,7 @@ const addFavorite = async (req,res) =>{
   }
 };
 //-------------------------------------------------------------------------------------------------------------------------------------
-const suggestTerm = async (req,res) =>{
-//   const newSuggest = new Suggest({
-//   "categories": [
-//     {
-//       "$numberInt": "32"
-//     }
-//   ],
-//   "shortDefinition": {
-//     "hebrew": "לינוקס היא מערכת הפעלה בקוד פתוח המורכבת מהקרנל, הרכיב הבסיסי של מערכת ההפעלה, ומהכלים, האפליקציות והשירותים המצורפים איתה.",
-//     "english": "Linux is an open source operating system that is made up of the kernel, the base component of the OS, and the tools, apps, and services bundled along with it.",
-//     "arabic": "لينكس هو نظام تشغيل مفتوح المصدر يتكون من النواة والمكون الأساسي لنظام التشغيل والأدوات والتطبيقات والخدمات المجمعة معه."
-//   },
-//   "lastEdited":  "1622382981234",
-//   "conceptName": {
-//     "arabic": "لينكس ",
-//     "hebrew": "לינוקס",
-//     "english": "Linux"
-//   },
-//   "lastEditedDisplayable": "Sun May 30 2021 16:56:25 GMT+0300",
-//   "longDefinition": {
-//     "english": "Linux or GNU/Linux is a cross-platform free software operating system created by Finnish Linus Tovalds. Its free software implies that it is not owned by any person or company and the source code is visible to anyone so it can be modified at the pleasure of anyone. It is also a cross-platform, multi-user and multitasking system, providing the command interface and the graphical interface.",
-//     "hebrew": "לינוקס או GNU/Linux היא מערכת הפעלה תוכנה חופשית חוצת פלטפורמות שנוצרה על ידי Linus Tovalds הפינית. התוכנה החינמית שלה מרמזת שהיא אינה בבעלות של אף אדם או חברה וקוד המקור גלוי לכל אחד כך שניתן לשנות אותו להנאתו של כל אחד. זוהי גם מערכת חוצת פלטפורמות, ריבוי משתמשים ומערכת ריבוי משימות, המספקת את ממשק הפקודה והממשק הגרפי.",
-//     "arabic": "Linux أو GNU / Linux هو نظام تشغيل برمجي مجاني عبر الأنظمة الأساسية تم إنشاؤه بواسطة الفنلندي Linus Tovalds. يشير برنامجها المجاني إلى أنها ليست مملوكة لأي شخص أو شركة وأن شفرة المصدر مرئية لأي شخص بحيث يمكن تعديلها حسب رغبة أي شخص. وهو أيضًا نظام متعدد المنصات ومتعدد المستخدمين ومتعدد المهام ، ويوفر واجهة الأوامر والواجهة الرسومية."
-//   },
-//   "suggestedBy": "mohamed sayed ahmad",
-//   "readMore": "https://he.wikipedia.org/wiki/%D7%9C%D7%99%D7%A0%D7%95%D7%A7%D7%A1",
-//   "firestore_id": "-MaxYwF9vxYYiTg0Is123"
-// });
-// const newSuggest = new Suggest({
-//   "categories": [
-//     {
-//       "$numberInt": "32"
-//     }
-//   ],
-//   "shortDefinition": {
-//     "hebrew": "לינוקס היא מערכת הפעלה בקוד פתוח המורכבת מהקרנל, הרכיב הבסיסי של מערכת ההפעלה, ומהכלים, האפליקציות והשירותים המצורפים איתה.",
-//     "english": "Linux is an open source operating system that is made up of the kernel, the base component of the OS, and the tools, apps, and services bundled along with it.",
-//   "arabic": "لينكس هو نظام تشغيل مفتوح المصدر يتكون من النواة والمكون الأساسي لنظام التشغيل والأدوات والتطبيقات والخدمات المجمعة معه.",
-    
-//   },
-//   "lastEdited":  "1622382981234",
-//   "conceptName": {
-//     "hebrew": "לינוקס",
-//     "english": "Linux",
-//     "arabic" : "لينكس"
-//   },
-//   "lastEditedDisplayable": "Sun May 30 2021 16:56:25 GMT+0300",
-//   "longDefinition": {
-//     "english": "Linux or GNU/Linux is a cross-platform free software operating system created by Finnish Linus Tovalds. Its free software implies that it is not owned by any person or company and the source code is visible to anyone so it can be modified at the pleasure of anyone. It is also a cross-platform, multi-user and multitasking system, providing the command interface and the graphical interface.",
-//     "hebrew": "לינוקס או GNU/Linux היא מערכת הפעלה תוכנה חופשית חוצת פלטפורמות שנוצרה על ידי Linus Tovalds הפינית. התוכנה החינמית שלה מרמזת שהיא אינה בבעלות של אף אדם או חברה וקוד המקור גלוי לכל אחד כך שניתן לשנות אותו להנאתו של כל אחד. זוהי גם מערכת חוצת פלטפורמות, ריבוי משתמשים ומערכת ריבוי משימות, המספקת את ממשק הפקודה והממשק הגרפי.",
-//   "arabic": "Linux أو GNU / Linux هو نظام تشغيل برمجي مجاني عبر الأنظمة الأساسية تم إنشاؤه بواسطة الفنلندي Linus Tovalds. يشير برنامجها المجاني إلى أنها ليست مملوكة لأي شخص أو شركة وأن شفرة المصدر مرئية لأي شخص بحيث يمكن تعديلها حسب رغبة أي شخص. وهو أيضًا نظام متعدد المنصات ومتعدد المستخدمين ومتعدد المهام ، ويوفر واجهة الأوامر والواجهة الرسومية.",
-    
-//   },
-//   "suggestedBy": "mohamed sayed ahmad",
-//   "readMore": "https://he.wikipedia.org/wiki/%D7%9C%D7%99%D7%A0%D7%95%D7%A7%D7%A1",
-//   "firestore_id": "-MaxYwF9vxYYiTg0Is123"
-// }
-//   );
-//   try{
-//     newSuggest.save();
-//     console.log(newSuggest.find());
-//     res.send(newSuggest.find());
-//   }catch(err){
-//     res.send(err);
-//   }
-// };
-//testing body
+
 const newSuggest = new Suggest({
   categories: req.body.categories,
     shortDefinition: req.body.shortDefinition,
