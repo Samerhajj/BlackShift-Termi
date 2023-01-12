52c5d2439d7b89540d702af073916dc91d04bb7d
Samerhajj <samerhaj44@gmail.com>
2023-01-12 08:53:47 +0000

    Added category api for selecting category and sending request to backend

---
+++ FrontEnd/termi-project/src/api/CategoryAPI.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { allCategoriesRoute } from '../api/ApiRoutes';
+
+
+const getAllCategories = async ()=>{
+    try{
+        const res = await axios.get(allCategoriesRoute);
+        return {body: res.data, success: true};
+    }catch(e){
+        if(e.response){
+            return {success: false, message: e.response.data};
+        }else{
+            return {success: false, message: e.message};
+        }
+    }
+};
+
+export default {getAllCategories};
