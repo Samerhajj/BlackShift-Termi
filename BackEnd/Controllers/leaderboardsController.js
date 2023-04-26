46f2d6e581483755d24b133281efad080546a047
samerhajj <samerhaj44@gmail.com>
2023-04-26 13:46:40 +0300

    Adding leaderboard logic

--- BackEnd/Controllers/leaderboardsController.js
+++ BackEnd/Controllers/leaderboardsController.js
@@ -3,70 +3,40 @@ const Leaderboard = require("../Models/leaderboardSchema");
 const jwt = require("jsonwebtoken");
 const mongoose = require('mongoose');
 
-// Function to get the suggestions leaderboard and the rank of the user sending the request
-const getSuggestionsLeaderboard = async(req, res, next) => {
-    try{
-        const token = req.headers['x-auth-token'];
-        const decoded = jwt.verify(token, process.env.SECRET);
-        const userId = decoded.id;
-        const limitNum = parseInt(req.query.limit, 10);
-        const category = req.query.category;
-        
-        const leaderboard = await User.aggregate([
-            { $match: { field: category } },
-            { $sort: { "suggestConceptCounter": -1 } },
-            { $project: {
-                _id: 0,
-                fullName: 1,
-                email: 1,
-                field: 1,
-                gender: 1,
-                suggestion: 1,
-                suggestConceptCounter: 1
-            }},
-            { $limit: limitNum }
-        ]);
-        res.status(200).json(leaderboard);
-    }catch(e){
-        console.log(e);
-        res.status(500).send("Server error");
-    }
-    
-};
-
 // Function to get the game leaderboard and the rank of the user sending the request
-const getGameLeaderboard = async(req, res, next) => {
+const getLeaderboard = async(req, res, next) => {
     try{
         const token = req.headers['x-auth-token'];
         const decoded = jwt.verify(token, process.env.SECRET);
         const userId = decoded.id;
         const category = req.query.category;
-        console.log(req.query);
-        console.log(token);
-        const gameName = req.query.gameName;
+        const context = req.query.context;
         const limit = req.query.limit;
-        if(category != null && gameName != null){
+        
+        console.log(req.query);
+        
+        if(category != null && context != null){
             let categoryNum = parseInt(category, 10);
             let limitNum = parseInt(limit, 10);
             
             const leaderboard = await Leaderboard.aggregate([
                                   // Find the category
                                   { $match: { categoryId: categoryNum } },
-                                  { $unwind: "$games" },
+                                  { $unwind: "$contexts" },
                                   // Find the Game
-                                  { $match: { "games.gameName": gameName } },
-                                  { $unwind: { path: "$games.leaderboard" } },
+                                  { $match: { "contexts.context": context } },
+                                  { $unwind: { path: "$contexts.leaderboard" } },
                                   // Sort them according to the score
-                                  { $sort: { "games.leaderboard.points": -1 } },
+                                  { $sort: { "contexts.leaderboard.points": -1 } },
                                   // Put add a new field with the user info according to the user's id
                                   { $lookup: {
                                       from: "userdatabases",
-                                      localField: "games.leaderboard.userId",
+                                      localField: "contexts.leaderboard.userId",
                                       foreignField: "_id",
                                       as: "user"}
                                   },
                                   { $addFields: {
-                                        "games.leaderboard.user": { 
+                                        "contexts.leaderboard.user": { 
                                             $let: {
                                                 vars: { user: { $arrayElemAt: ["$user", 0] } },
                                                 in: { fullName: "$$user.fullName", email: "$$user.email", field: "$$user.field", gender: "$$user.gender" }
@@ -77,14 +47,14 @@ const getGameLeaderboard = async(req, res, next) => {
                                   { $group: {
                                       _id: "$_id", 
                                       categoryId: { $first: "$categoryId" },
-                                      gameName: { $first: "$games.gameName" },
-                                      leaderboard: { $push: "$games.leaderboard" } }
+                                      context: { $first: "$contexts.context" },
+                                      leaderboard: { $push: "$contexts.leaderboard" } }
                                   },
                                   // Return the wanted fields with the userRank
                                   { $project: {
                                       _id: 0,
                                       categoryId: 1,
-                                      gameName: 1,
+                                      context: 1,
                                       userRank: {
                                         $add: [{$indexOfArray: [{ $map: {
                                                             input: "$leaderboard",
@@ -111,8 +81,7 @@ const getGameLeaderboard = async(req, res, next) => {
             if(!leaderboard){
                 res.status(400).json({ message:"Leaderboard is empty" });
             }else{
-                res.status(200).json(leaderboard);
-        
+                res.status(200).json(leaderboard[0]);
             }
         }else{
             res.status(400).json({ message: "Incorrect or missing data"});
@@ -123,116 +92,87 @@ const getGameLeaderboard = async(req, res, next) => {
     }
 };
 
-// Function to test the addition of game points using postman
-const addGamePoints = async(req, res, next) => {
-    // This Funciton is not in use its for Postman test only
-    const token = req.headers['x-auth-token'];
-    const category = req.body.category;
-    const gameName = req.body.gameName;
-    const points = req.body.points;
-    
-    addLeaderboardPoints(token, category, gameName, points);
-    res.status(200).json("Leaderboard Updated");
-    
-    // OLD CODE:
-    // try{
-    //     const token = req.headers['x-auth-token'];
-    //     const decoded = jwt.verify(token, process.env.SECRET);
-    //     const category = req.body.category;
-    //     const gameName = req.body.gameName;
-    //     const userId = decoded.id;
-    //     const points = req.body.points;
+// Function to get all the names of leaderboards available in a category
+const getAvailableContexts = async(req, res, next) => {
+    try{
+        const category = req.query.category;
         
-    //     if(category != null && gameName != null && userId != null && points != null){
-    //         let categoryNum = parseInt(category, 10);
-    //         let pointsAmmount = parseInt(points, 10);
-    //         // Find the leaderboard with the specified category id
-    //         const leaderboard = await Leaderboard.findOne({ "categoryId": categoryNum });
-    //         if(!leaderboard){
-    //             const newLeaderboard = new Leaderboard({
-    //                   "categoryId": categoryNum,
-    //                   games: [{ "gameName": gameName,
-    //                             "leaderboard": [{ "userId": userId,
-    //                                               "points": pointsAmmount }]
-    //                   }]
-    //                 });
-    //                 newLeaderboard.save();
-    //         }else{
-    //             // Category found, find the game with the specified name
-    //             const game = leaderboard.games.find((game) => game.gameName === gameName);
-    //             if (!game) {
-    //               // Game not found, create new game and add user to leaderboard
-    //               leaderboard.games.push({ "gameName": gameName, "leaderboard": [{ "userId": userId, "points": pointsAmmount }] });
-    //               leaderboard.save();
-    //             } else {
-    //               // Game found, find the user with the specified id
-    //               const user = game.leaderboard.find((user) => user.userId.toString() === userId);
-    //               if (!user) {
-    //                 // User not found, add to leaderboard with default points value of 0
-    //                 game.leaderboard.push({ "userId": userId, "points": pointsAmmount });
-    //                 leaderboard.save();
-    //               } else {
-    //                 // User already exists in leaderboard
-    //                 user.points+=pointsAmmount;
-    //                 leaderboard.save()
-    //               }
-    //             }
-    //         }
-    //         res.status(200).json({ message: "Points updated successfully"});
-    //     }else{
-    //         res.status(400).json({ message: "Incorrect or missing data"});
-    //     } 
-    // }catch(e){
-    //     console.log(e);
-    //     res.status(500).send("Server error");
-    // }
-};
-
-// Function to add points to a user in the game leaderboard
-const addLeaderboardPoints = async(token, category, gameName, points) => {
-    const decoded = jwt.verify(token, process.env.SECRET);
-    const userId = decoded.id;
-    if(category != null && gameName != null && userId != null && points != null){
-        let categoryNum = parseInt(category, 10);
-        let pointsAmmount = parseInt(points, 10);
-        // Find the leaderboard with the specified category id
-        const leaderboard = await Leaderboard.findOne({ "categoryId": categoryNum });
-        if(!leaderboard){
-            const newLeaderboard = new Leaderboard({
-                  "categoryId": categoryNum,
-                  games: [{ "gameName": gameName,
-                            "leaderboard": [{ "userId": userId,
-                                              "points": pointsAmmount }]
-                  }]
-                });
-                newLeaderboard.save();
+        if(category != null){
+            let categoryNum = parseInt(category, 10);
+            
+            const data = await Leaderboard.aggregate([
+                                { 
+                                    $match: { categoryId: categoryNum }
+                                },
+                                {
+                                    $project: {
+                                        _id: 0,
+                                        contexts: "$contexts.context"
+                                    }
+                                }
+                                ]);
+            if(!data){
+                res.status(400).json({ message:"There are no contexts for this category" });
+            }else{
+                console.log(data)
+                res.status(200).json(data[0]);
+            }
         }else{
-            // Category found, find the game with the specified name
-            const game = leaderboard.games.find((game) => game.gameName === gameName);
-            if (!game) {
-              // Game not found, create new game and add user to leaderboard
-              leaderboard.games.push({ "gameName": gameName, "leaderboard": [{ "userId": userId, "points": pointsAmmount }] });
-              leaderboard.save();
-            } else {
-              // Game found, find the user with the specified id
-              const user = game.leaderboard.find((user) => user.userId.toString() === userId);
-              if (!user) {
-                // User not found, add to leaderboard with default points value of 0
-                game.leaderboard.push({ "userId": userId, "points": pointsAmmount });
-                leaderboard.save();
-              } else {
-                // User already exists in leaderboard
-                user.points+=pointsAmmount;
-                leaderboard.save();
-              }
+            res.status(400).json({ message: "Incorrect or missing data"});
+        }   
+    }catch(e){
+        console.log(e);
+        res.status(500).send("Server error");
+    }
+}
+
+// Function to add points to a user in the leaderboard
+const addLeaderboardPoints = async(userId, category, context, points) => {
+    try{
+        if(category != null && context != null && userId != null && points != null){
+            let categoryNum = parseInt(category, 10);
+            let pointsAmmount = parseInt(points, 10);
+            
+            // Find the leaderboard with the specified category id
+            const leaderboard = await Leaderboard.findOne({ "categoryId": categoryNum });
+            if(!leaderboard){
+                const newLeaderboard = new Leaderboard({
+                      "categoryId": categoryNum,
+                      contexts: [{ "context": context,
+                                "leaderboard": [{ "userId": userId,
+                                                  "points": pointsAmmount }]
+                      }]
+                    });
+                    newLeaderboard.save();
+            }else{
+                // Category found, find the context provided
+                const contextRes = leaderboard.contexts.find((item) => item.context === context);
+                if (!contextRes) {
+                  // Context not found, create new context and add user to leaderboard
+                  leaderboard.contexts.push({ "context": context, "leaderboard": [{ "userId": userId, "points": pointsAmmount }] });
+                  leaderboard.save();
+                } else {
+                  // Context found, find the user with the specified id
+                  const user = contextRes.leaderboard.find((user) => user.userId.toString() === userId);
+                  if (!user) {
+                    // User not found, add to leaderboard with the ammount of points provided
+                    contextRes.leaderboard.push({ "userId": userId, "points": pointsAmmount });
+                    leaderboard.save();
+                  } else {
+                    // User already exists in leaderboard
+                    user.points+=pointsAmmount;
+                    leaderboard.save();
+                  }
+                }
             }
         }
+    }catch(error){
+        console.log(error);
     }
 };
 
 module.exports={
-    getSuggestionsLeaderboard,
-    getGameLeaderboard,
-    addGamePoints,
-    addLeaderboardPoints
+    getLeaderboard,
+    addLeaderboardPoints,
+    getAvailableContexts
 };
