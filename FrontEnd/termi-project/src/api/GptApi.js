8c7a42faf874786875da06b45267ef37ac69e0e0
samerhajj <samerhaj44@gmail.com>
2023-06-17 10:51:07 +0300

    Added gpt pai

---
+++ FrontEnd/termi-project/src/api/GptApi.js
@@ -0,0 +1,87 @@
+import axios from "axios";
+
+const API_KEY =process.env.REACT_APP_OPEN_API_KEY;
+console.log(API_KEY);
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: API_KEY,
+  
+});
+const openai = new OpenAIApi(configuration);
+
+const PROMPT = `Generate a JSON object containing definitions for a specific term in different languages. The JSON object should include conceptName, shortDefinition, and longDefinition in English, Arabic, and Hebrew.
+
+User Input: [UserTerm]
+
+Example Output Format:
+{
+  "conceptName": {
+    "english": "[Term in English]",
+    "arabic": "[Term in Arabic]",
+    "hebrew": "[Term in Hebrew]"
+  },
+  "shortDefinition": {  
+    "english": "[Short Definition in English]",
+    "arabic": "[Short Definition in Arabic]",
+    "hebrew": "[Short Definition in Hebrew]"
+  },
+  "longDefinition": {
+    "english": "[Long Definition in English]",
+    "arabic": "[Long Definition in Arabic]",
+    "hebrew": "[Long Definition in Hebrew]"
+  }
+  
+}
+
+No need to give additional information. just the output. no need to say anything more. , and dont forget to give exact output format. with all parenthesis required`;
+const sendRequest = async (userInput) => {
+  try {
+    console.log(process.env.OPENAI_API_KEY);
+
+    
+    const userPrompt=PROMPT.replace("[UserTerm]",userInput);
+    console.log(userPrompt);
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: userPrompt },
+      ],
+    });
+  
+    const reply = completion.data.choices[0].message.content;
+    return reply;
+  } catch (error) {
+    console.error(error);
+  }
+};
+// const sendRequest = async (userInput) => {
+//   try {
+//     const userPrompt = PROMPT.replace("[UserTerm]", userInput);
+//     console.log(userPrompt);
+//     const completion = await openai.createChatCompletion({
+//       model: "gpt-3.5-turbo",
+//       messages: [
+//         { role: "system", content: "You are a helpful assistant." },
+//         { role: "user", content: userPrompt },
+//       ],
+//     });
+
+//     const reply = completion.data.choices[0].message.content;
+//     const response = JSON.parse(reply);
+
+//     const shortDefinition = {
+//       english: response.shortDefinition.english || "",
+//       arabic: response.shortDefinition.arabic || "",
+//       hebrew: response.shortDefinition.hebrew || "",
+//     };
+
+//     return shortDefinition;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+
+export default {sendRequest};
---
+++ FrontEnd/termi-project/src/pages/ChatGptPage.js
@@ -0,0 +1,143 @@
+import React, { useState, useContext } from 'react';
+import { Form, Input, Button, Spin,Select  } from 'antd';
+import GptApi from '../api/GptApi';
+import TermCard from '../components/TermCard/TermCard';
+import CategorySelector from '../components/CategorySelector';
+
+// Contexts
+import { CategoriesContext } from "../components/CategoryContext";
+
+const { Option } = Select;
+const ChatGptPage = () => {
+  const { categories } = useContext(CategoriesContext);
+  const [form] = Form.useForm();
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+const [gptConcept,setGptConcept]=useState();
+  const handleFormChange = () => {
+    const { getFieldValue } = form;
+    const field1 = getFieldValue('field1');
+    const field2 = getFieldValue('field2');
+    setButtonDisabled(!field1);
+  };
+  const handleButtonClick = async () => {
+    try {
+      const { getFieldValue } = form;
+      const termName = getFieldValue('field1');
+      const field2 = getFieldValue('field2');
+      
+      const category = categories.find(categry => categry.categoryId == field2);
+      
+      
+      // Perform your logic here with the form values
+      setLoading(true);
+      setButtonDisabled(true); // Disable the button
+      const concept = await GptApi.sendRequest(termName);
+      setLoading(false);
+      //console.log('Response from the API:\n', concept);
+      
+// Deconstruct the response object
+ const parsedConcept  = JSON.parse(concept);
+
+ // Access the individual values
+    const termEnglish = parsedConcept.conceptName.english;
+    const termArabic = parsedConcept.conceptName.arabic;
+    const termHebrew = parsedConcept.conceptName.hebrew;
+
+    const shortDefEnglish = parsedConcept.shortDefinition.english;
+    const shortDefArabic = parsedConcept.shortDefinition.arabic;
+    const shortDefHebrew = parsedConcept.shortDefinition.hebrew;
+
+    const longDefEnglish = parsedConcept.longDefinition.english;
+    const longDefArabic = parsedConcept.longDefinition.arabic;
+    const longDefHebrew = parsedConcept.longDefinition.hebrew;
+    
+    parsedConcept.categories = {data:[category.categoryName]};
+
+    // Print the individual values
+    console.log("Term in English:", termEnglish);
+    console.log("Term in Arabic:", termArabic);
+    console.log("Term in Hebrew:", termHebrew);
+
+    console.log("Short Definition in English:", shortDefEnglish);
+    console.log("Short Definition in Arabic:", shortDefArabic);
+    console.log("Short Definition in Hebrew:", shortDefHebrew);
+
+    console.log("Long Definition in English:", longDefEnglish);
+    console.log("Long Definition in Arabic:", longDefArabic);
+    console.log("Long Definition in Hebrew:", longDefHebrew);
+    
+    console.log(concept);
+    setGptConcept(parsedConcept);
+      // Handle the concept response as needed
+    } catch (error) {
+      setLoading(false);
+      console.error('Error fetching concept from the API:', error);
+    } finally {
+      setButtonDisabled(false); // Enable the button
+    }
+  };
+
+  const handleCategoryChange = value => {
+    console.log(value); // Log the value of field2
+  };
+  return (
+    <>
+      <div className="banner banner_game">
+        <div className='wrapper'>
+          <div className="banner_content" />
+        </div>
+      </div>
+         {console.log(form.getFieldValue('field2'))}
+      <h1>Ask ChatGPT</h1>
+          {console.log(categories)}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', marginTop: '-200px', background: '#f0f2f5' }}>
+        <div>
+          <Form form={form} onFieldsChange={handleFormChange}>
+            <Form.Item name="field1" rules={[{ required: true, message: 'Field 1 is required' }]}>
+              <Input placeholder="Concept Name" />
+            </Form.Item>
+             <Form.Item name="field2" rules={[{ required: true, message: 'Field 2 is required' }]}>
+              <Select
+                placeholder="Select a category"
+                onChange={handleCategoryChange}
+                allowClear
+              >
+                {categories.map((category, index) => {
+                  const categoryName = category.categoryName.english;
+                  return (
+                    <Option key={index} value={category.categoryId}>
+                      {categoryName}
+                    </Option>
+                  );
+                })}
+              </Select>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" onClick={handleButtonClick} disabled={buttonDisabled}>
+                Submit
+              </Button>
+            </Form.Item>
+            
+          </Form>
+        {loading && <Spin />}
+{gptConcept ? (
+  <div>
+    <TermCard
+      role={"role"}
+      term={gptConcept}
+      isSearch={true}
+      isFavorite={"true"}
+      initialLanguage={"en"}
+      //categories={{data: [{"english": "Human Resources","arabic":"الموارد البشرية","hebrew":"משאבי אנוש"}]}}
+      categories={gptConcept.categories}
+    />
+  </div>
+) : null}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ChatGptPage;
