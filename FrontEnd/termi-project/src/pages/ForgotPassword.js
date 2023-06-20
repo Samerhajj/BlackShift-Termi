7a4f92469657d06f02ec4f6abd5ac2ec515b2a65
samerhajj <samerhaj44@gmail.com>
2023-06-20 15:16:59 +0300

    Fixed forgotpassword

--- FrontEnd/termi-project/src/pages/ForgotPassword.js
+++ FrontEnd/termi-project/src/pages/ForgotPassword.js
@@ -2,25 +2,45 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useTranslation } from 'react-i18next';
 import AuthAPI from '../api/AuthAPI';
+import validator from 'validator';
 
 const ForgotPassword = () =>{
  const {t} = useTranslation();
  const [email, setEmail] = useState("");
  const [requestSent, setRequestSent] = useState(false);
- const [errorMessage, setErrorMessage] = useState("");
+ 
+  const [error, setError] = useState({
+    validEmail: true,
+    notExistMail: false
+  });
 
    const handleResetPassword = async () => {
+      if (email === '') {
+      setError({ ...error, validEmail: false,notExistMail:false});
+      console.log("hi")
+      // return;
+    }//if the email is empty
+     if (!validator.isEmail(email)) {
+      setError({ ...error, validEmail: false,notExistMail:false});
+      return;
+     } else {
+      setError({ ...error, validEmail: true,notExistMail:false});
+    }
+
     try {
       const res = await AuthAPI.forgotPassword({email})
-      if (res.status === 404) {
-        setErrorMessage("User not found");
+      console.log(res)
+      if (res.message === "Request failed with status code 404") {
+           setError({ ...error, notExistMail: true ,validEmail:true });
+          console.log("not exist mail")
+
       } else {
         setRequestSent(true);
       }
     } catch (err) {
       console.log(err);
     }
-  };
+  };// END handleResetPassword
   
    const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -34,6 +54,7 @@ const ForgotPassword = () =>{
       </div>
       </div>
       </div>
+
      <div className="d-flex justify-content-center">
            <div className="">
              <h1 className="">{t('forgot_password.forgot_password')}</h1>
@@ -44,7 +65,7 @@ const ForgotPassword = () =>{
              ) : (
              <>
              <h6 className ="information-text">{t('forgot_password.text')}</h6>
-             <input 
+             <input
              className="form-control rounded-left"
              type="email"
              name="user_email"
@@ -52,9 +73,11 @@ const ForgotPassword = () =>{
              onChange={handleEmailChange}
              value={email}
              placeholder={t('forgot_password.email')}/>
-             {errorMessage && <div className="alert alert-danger" role="alert">{errorMessage}</div>}
-             <button onClick={handleResetPassword} className="d-block btn btn-primary mt-5 w-100">{t('forgot_password.reset_password')}</button>
+             <button id="id_rest_password_button" onClick={handleResetPassword} className="d-block btn btn-primary mt-5 w-100">{t('forgot_password.reset_password')}</button>
              </>)}
+             {error['validEmail'] ? null : <label id="id_error_message_rest_password_validEmail" className="text-danger"> Not valid email </label> }
+             {!error['notExistMail'] ? null : <label id="id_error_message_rest_password_notExistMail" className="text-danger">your mail not in our database</label> }
+
              </div>
        </div>
        </>
